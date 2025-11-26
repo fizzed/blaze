c9c3282c3a95f3961c2340b471af5e2ac8c38381
Joe Lauer <joe@lauer.bz>
2025-11-26 09:59:07 -0500

    Improve jsync demo

--- blaze-jsync/src/test/java/com/fizzed/blaze/jsync/JsyncDemo.java
+++ blaze-jsync/src/test/java/com/fizzed/blaze/jsync/JsyncDemo.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import java.nio.file.Paths;
 
+import static com.fizzed.blaze.Contexts.withUserDir;
 import static com.fizzed.blaze.jsync.Jsyncs.*;
 
 public class JsyncDemo {
@@ -19,23 +20,18 @@ public class JsyncDemo {
         LoggerConfig.setDefaultLogLevel(LogLevel.INFO);
 //        LoggerConfig.setDefaultLogLevel(LogLevel.DEBUG);
 
-//        final String sourceDir = Paths.get("/home/jjlauer/test-sync").toString();
-//        final String sourceDir = Paths.get("/home/jjlauer/workspace/third-party/jsch").toString();
-//        final String sourceDir = Paths.get("/home/jjlauer/workspace/third-party/coredns").toString();
-        final VirtualVolume source = localVolume(Paths.get("/home/jjlauer/workspace/third-party/nats.java"));
-//        final VirtualVolume source = localVolume(Paths.get("/home/jjlauer/workspace/third-party/tokyocabinet-1.4.48"));
-//        final VirtualVolume source = localVolume(Paths.get("/home/jjlauer/Downloads"));
-//        final String sourceDir = Paths.get("C:\\Users\\jjlauer\\test-sync").toString();
-//        final String sourceDir = Paths.get("C:\\Users\\jjlauer\\workspace\\third-party\\tokyocabinet-1.4.48").toString();
-//        final String sourceDir = Paths.get("/home/jjlauer/workspace/third-party/tokyocabinet-1.4.48").toString();
-//        final String sourceDir = Paths.get("/home/jjlauer/Downloads/haiku-r1beta5-x86_64-anyboot.iso").toString();
+//        final VirtualVolume source = localVolume(withUserDir("workspace/third-party/coredns"));
+//        final VirtualVolume source = localVolume(withUserDir("workspace/third-party/jsch"));
+//        final VirtualVolume source = localVolume(withUserDir("workspace/third-party/nats.java"));
+//        final VirtualVolume source = localVolume((withUserDir("workspace/third-party/tokyocabinet-1.4.48"));
+        final VirtualVolume source = localVolume(withUserDir("Downloads"));
 
 //        final String sshHost = "bmh-dev-x64-indy25-1";
 //        final String sshHost = "bmh-dev-x64-fedora43-1";
 //        final String sshHost = "bmh-build-x64-freebsd15-1";
-        final VirtualVolume target = sftpVolume("bmh-build-x64-win11-1", "test-sync");
+//        final VirtualVolume target = sftpVolume("bmh-build-x64-win11-1", "test-sync");
 //        final VirtualVolume target = sftpVolume("bmh-dev-x64-indy25-1", "test-sync");
-//        final VirtualVolume target = sftpVolume("bmh-dev-x64-fedora43-1", "test-sync");
+        final VirtualVolume target = sftpVolume("bmh-dev-x64-fedora43-1", "test-sync");
 
 
         final JsyncResult result = jsync(source, target, JsyncMode.MERGE)
