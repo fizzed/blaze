aeb716bd82aff35888fbafe69b3f584460eb8c39
Joe Lauer <joe@lauer.bz>
2025-12-03 12:32:01 -0500

    Bump jsch to v2.27.7 (has public SftpATTRS constructor)

--- blaze-ssh/src/main/java/com/fizzed/blaze/ssh/impl/JschSftpSession.java
+++ blaze-ssh/src/main/java/com/fizzed/blaze/ssh/impl/JschSftpSession.java
@@ -416,50 +416,9 @@ public class JschSftpSession extends SshSftpSession implements SshSftpSupport {
         }
     }
 
-
-    static private final AtomicReference<Constructor<SftpATTRS>> SFTP_ATTRS_CONSTRUCTOR_REF = new AtomicReference<>();
-
-    static protected SftpATTRS createSftpATTRS() {
-        Constructor<SftpATTRS> sftpATTRSConstructor = SFTP_ATTRS_CONSTRUCTOR_REF.get();
-
-        if (sftpATTRSConstructor == null) {
-            synchronized (SFTP_ATTRS_CONSTRUCTOR_REF) {
-                sftpATTRSConstructor = SFTP_ATTRS_CONSTRUCTOR_REF.get();
-                // double lock
-                if (sftpATTRSConstructor == null) {
-                    try {
-                        // SftpATTRS has private access for some fucking reason (no idea why)
-                        // can we use reflection instead?
-                        // 2. Get the Class object
-                        Class<SftpATTRS> clazz = SftpATTRS.class;
-
-                        // 3. Get the specific constructor
-                        // Note: Use getDeclaredConstructor(), NOT getConstructor()
-                        // getConstructor() only finds PUBLIC constructors.
-                        Constructor<SftpATTRS> constructor = clazz.getDeclaredConstructor();
-
-                        // 4. The "Magic" Step: Force access
-                        constructor.setAccessible(true);
-
-                        sftpATTRSConstructor = constructor;
-                        SFTP_ATTRS_CONSTRUCTOR_REF.set(sftpATTRSConstructor);
-                    } catch (Throwable t) {
-                        throw new RuntimeException("Unable to use reflection to get SftpATTRS instance", t);
-                    }
-                }
-            }
-        }
-
-        try {
-            return sftpATTRSConstructor.newInstance();
-        } catch (Throwable t) {
-            throw new RuntimeException("Unable to create SftpATTRS instance", t);
-        }
-    }
-
     @Override
     public void attrs(String path, Integer uid, Integer gid, Integer permissions, Integer mtime, Integer atime) {
-        SftpATTRS attrs = createSftpATTRS();
+        SftpATTRS attrs = new SftpATTRS();
 
         // handle setting uid & gid
         if (uid != null || gid != null) {
